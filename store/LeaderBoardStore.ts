069289f23d3ac8b52e741d4bbba1e6d91a041d7c
Krisztian Szalai <krisztwos@gmail.com>
2022-10-27 23:19:56 +0200

    add store

---
+++ store/LeaderBoardStore.ts
@@ -0,0 +1,33 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import create from "zustand";
+import { persist } from "zustand/middleware";
+import { User } from "../types";
+
+interface LeaderboardState {
+  data: User[];
+  setData: (value: User[]) => void;
+  filterText: string;
+  setFilterText: (text: string) => void;
+  initStore: () => void;
+}
+
+export const useDeliveryStore = create(
+  persist<LeaderboardState>(
+    (set) => ({
+      data: [],
+      setData: (value: User[]) => set({ data: value }),
+      filterText: "",
+      setFilterText: (text: string) => set({ filterText: text }),
+
+      initStore: () =>
+        set({
+          data: [],
+          filterText: "",
+        }),
+    }),
+    {
+      name: "leaderboard-store",
+      getStorage: () => AsyncStorage,
+    }
+  )
+);
